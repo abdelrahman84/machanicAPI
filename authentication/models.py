4075797b85586ee0e922abf70a4b5ca0bcc22a8b
abdelrahman84 <abdelrahman.farag84@gmail.com>
2021-05-29 03:58:54 +0200

    adding validation for user model

--- authentication/models.py
+++ authentication/models.py
@@ -1,18 +1,28 @@
+from django.core import validators
 from django.db import models
+from django.contrib.auth.validators import ASCIIUsernameValidator
 
 # Create your models here.
 
 
 class User(models.Model):
+
+    username_validatior = ASCIIUsernameValidator()
+
     first_name = models.CharField(
         "First name", max_length=255, blank=False, null=False)
     last_name = models.CharField(
         "Last name", max_length=255, blank=False, null=False)
 
     username = models.CharField(
-        "username", max_length=255, blank=False, null=False, unique=True)
+        "username", max_length=255, blank=False, null=False, unique=True, validators=[username_validatior])
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20, blank=False, null=False)
+    phone = models.CharField(max_length=20, blank=False, null=False, validators=[
+        validators.RegexValidator(
+            regex='^(?=\d{11,}$)(01)\d',
+            message='Please enter a valid number'
+        )
+    ])
     createdAt = models.DateTimeField("Created At", auto_now_add=True)
 
     def __str__(self):
