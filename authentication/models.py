d8069f075b03923b8d838624a6509a12ce64388c
abdelrahman84 <abdelrahman.farag84@gmail.com>
2021-06-02 18:30:43 +0200

    add password, email_verified to user

--- authentication/models.py
+++ authentication/models.py
@@ -7,23 +7,35 @@ from django.contrib.auth.validators import ASCIIUsernameValidator
 
 class User(models.Model):
 
-    username_validatior = ASCIIUsernameValidator()
+	username_validatior = ASCIIUsernameValidator()
 
-    first_name = models.CharField(
-        "First name", max_length=255, blank=False, null=False)
-    last_name = models.CharField(
-        "Last name", max_length=255, blank=False, null=False)
+	first_name = models.CharField(
+		"First name", max_length=255, blank=False, null=False)
+	last_name = models.CharField(
+		"Last name", max_length=255, blank=False, null=False)
 
-    username = models.CharField(
-        "username", max_length=255, blank=False, null=False, unique=True, validators=[username_validatior])
-    email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20, blank=False, null=False, validators=[
-        validators.RegexValidator(
-            regex='^(?=\d{11,}$)(01)\d',
-            message='Please enter a valid number'
-        )
-    ])
-    createdAt = models.DateTimeField("Created At", auto_now_add=True)
+	username = models.CharField(
+		"username", max_length=255, blank=False, null=False, unique=True, validators=[username_validatior])
 
-    def __str__(self):
-        return self.first_name
+	email = models.EmailField(unique=True)
+
+	phone = models.CharField(max_length=225, blank=False, null=False, validators=[
+		validators.RegexValidator(
+			regex='^(?=\d{11,}$)(01)\d',
+			message='Please enter a valid number'
+		)
+	])
+
+	email_verified = models.BooleanField(default=False)
+
+	password = models.CharField(max_length=128, blank=True, null=False, validators=[
+		validators.RegexValidator(
+			regex='^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[@#$])[\w\d@#$]{8,}$',
+			message='Please enter a strong password'
+		)
+	])
+
+	createdAt = models.DateTimeField("Created At", auto_now_add=True)
+
+	def __str__(self):
+		return self.first_name
